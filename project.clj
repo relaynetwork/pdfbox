be074145831009284d7fa70b0aeda7f754c68de3
Kyle Burton <kburton@relaynetwork.com>
2011-01-26 18:29:07 -0500

    initial import

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject pdfbox "1.0.0-SNAPSHOT"
+  :description "Sandbox for testing out Apache Jakarta's PDFBox Library: http://pdfbox.apache.org/"
+  :dev-dependencies [[swank-clojure "1.2.1"]]
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [org.apache.pdfbox/pdfbox "1.4.0"] ])
